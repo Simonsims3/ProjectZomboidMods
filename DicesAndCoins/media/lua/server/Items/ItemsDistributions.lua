7103bfe43f088f21fa72999355a065058be184c1
SimoN <31354321+Simonsims3@users.noreply.github.com>
2022-02-06 01:59:25 +0100

    Add files via upload

---
+++ DicesAndCoins/media/lua/server/Items/ItemsDistributions.lua
@@ -0,0 +1,4 @@
+require 'Items/Distributions'
+
+table.insert(SuburbsDistributions["all"]["cashregister"].items, "simonRandom.coinItem");
+table.insert(SuburbsDistributions["all"]["cashregister"].items, 50.0);
